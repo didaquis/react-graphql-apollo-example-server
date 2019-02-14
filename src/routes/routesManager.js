fd56bde2b38ba36775e7dc5b985ea821e2599a5e
Dídac García <didaquis@gmail.com>
2019-02-14 21:15:52 +0100

    Added first schema and resolver

--- src/routes/routesManager.js
+++ src/routes/routesManager.js
@@ -6,4 +6,18 @@ routesManager.get('/', (req, res) => {
 	res.send('WIP');
 });
 
+
+const graphqlHTTP = require('express-graphql');
+const schema = require('../schema/schema'); /* GraphQL schema */
+const root = { hola: () => 'Hola mundo desde GraphQL' }; /* GraphQL resolver */
+
+routesManager.use('/graphql', graphqlHTTP(
+	{
+		schema: schema, // schema a utilizar
+		rootValue: root, // resolver a utilizar
+		graphiql: true // habilitamos graphiql
+
+	}
+));
+
 module.exports = routesManager;
---
+++ src/schema/schema.js
@@ -0,0 +1,9 @@
+const { buildSchema } = require('graphql');
+
+const schema = buildSchema(`
+	type Query {
+		hola: String
+	}
+`);
+
+module.exports = schema;
