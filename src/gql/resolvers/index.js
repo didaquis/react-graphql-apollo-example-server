94ca2d87b57c6729787ba6c4cd15fa3367c6baf7
Dídac García <didaquis@gmail.com>
2019-03-28 10:46:49 +0100

    Added resolvers for create top 10 customers chart

--- src/gql/resolvers/index.js
+++ src/gql/resolvers/index.js
@@ -3,9 +3,11 @@ const { merge } = require('lodash');
 const clientes = require('./clientes');
 const productos = require('./productos');
 const pedidos = require('./pedidos');
+const graficas	 = require('./graficas');
 
 module.exports = merge(
 	clientes,
 	productos,
-	pedidos
+	pedidos,
+	graficas
 );
